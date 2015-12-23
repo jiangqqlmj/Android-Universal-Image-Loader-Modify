767c2efea6e8fa88d058a4053b9aa9e16d84bbfe
jiangqqlmj <781931404@qq.com>
2015-12-23 15:18:23 +0800

    +):1.完成ImageLoader加载显示引擎类和相关方法的注释

--- library/src/main/java/com/nostra13/universalimageloader/core/ImageLoaderEngine.java
+++ library/src/main/java/com/nostra13/universalimageloader/core/ImageLoaderEngine.java
@@ -33,61 +33,84 @@ import java.util.concurrent.locks.ReentrantLock;
 
 /**
  *
+ * 图片加载引擎，用于执行图片加载和显示任务
  * {@link ImageLoader} engine which responsible for {@linkplain LoadAndDisplayImageTask display task} execution.
  *
  * @author Sergey Tarasevich (nostra13[at]gmail[dot]com)
  * @since 1.7.1
  */
 class ImageLoaderEngine {
-
+	/*ImageLoader加载配置*/
 	final ImageLoaderConfiguration configuration;
-
+	/*任务执行者*/
 	private Executor taskExecutor;
+	/*图片缓存任务执行则*/
 	private Executor taskExecutorForCachedImages;
+	/*任务分配者*/
 	private Executor taskDistributor;
 
 	private final Map<Integer, String> cacheKeysForImageAwares = Collections
 			.synchronizedMap(new HashMap<Integer, String>());
 	private final Map<String, ReentrantLock> uriLocks = new WeakHashMap<String, ReentrantLock>();
-
+	/*暂停*/
 	private final AtomicBoolean paused = new AtomicBoolean(false);
+	/*网络拒绝访问*/
 	private final AtomicBoolean networkDenied = new AtomicBoolean(false);
+	/*网络慢*/
 	private final AtomicBoolean slowNetwork = new AtomicBoolean(false);
 
 	private final Object pauseLock = new Object();
 
+	/**
+	 * ImageLoader引擎构造器
+	 * @param configuration
+	 */
 	ImageLoaderEngine(ImageLoaderConfiguration configuration) {
+		//初始化ImageLoader配置参数
 		this.configuration = configuration;
-
+        //初始化三个不同任务的执行者
 		taskExecutor = configuration.taskExecutor;
 		taskExecutorForCachedImages = configuration.taskExecutorForCachedImages;
-
 		taskDistributor = DefaultConfigurationFactory.createTaskDistributor();
 	}
 
 	/** Submits task to execution pool */
+	/**
+	 * 提交图片加载和显示任务到执行线程池中,进行运行
+	 * @param task   具体需要执行的任务
+	 */
 	void submit(final LoadAndDisplayImageTask task) {
 		taskDistributor.execute(new Runnable() {
 			@Override
 			public void run() {
+				//从文件系统缓存中获取图片文件
 				File image = configuration.diskCache.get(task.getLoadingUri());
+				//判断是否已经取得了图片
 				boolean isImageCachedOnDisk = image != null && image.exists();
 				initExecutorsIfNeed();
 				if (isImageCachedOnDisk) {
+					//如果当前图片已经缓存在本地文件系统了，直接采用taskExecutorForCachedImages来进行执行任务
 					taskExecutorForCachedImages.execute(task);
 				} else {
+					//当天图片在本地文件系统中没有缓存，直接采用taskExecutor来进行执行任务
 					taskExecutor.execute(task);
 				}
 			}
 		});
 	}
 
-	/** Submits task to execution pool */
+	/**
+	 * Submits task to execution pool
+	 * 提交图片显示任务并且执行 (该图片从内存缓存中取得)
+	 */
 	void submit(ProcessAndDisplayImageTask task) {
 		initExecutorsIfNeed();
 		taskExecutorForCachedImages.execute(task);
 	}
 
+	/**
+	 * 根据需要进行初始化执行者
+	 */
 	private void initExecutorsIfNeed() {
 		if (!configuration.customExecutor && ((ExecutorService) taskExecutor).isShutdown()) {
 			taskExecutor = createTaskExecutor();
@@ -98,6 +121,10 @@ class ImageLoaderEngine {
 		}
 	}
 
+	/**
+	 * 进行创建任务执行者
+	 * @return
+	 */
 	private Executor createTaskExecutor() {
 		return DefaultConfigurationFactory
 				.createExecutor(configuration.threadPoolSize, configuration.threadPriority,
@@ -105,6 +132,7 @@ class ImageLoaderEngine {
 	}
 
 	/**
+	 * 获取当前被加载ImageAware到图片的地址
 	 * Returns URI of image which is loading at this moment into passed {@link com.nostra13.universalimageloader.core.imageaware.ImageAware}
 	 */
 	String getLoadingUriForView(ImageAware imageAware) {
@@ -112,6 +140,7 @@ class ImageLoaderEngine {
 	}
 
 	/**
+	 *
 	 * Associates <b>memoryCacheKey</b> with <b>imageAware</b>. Then it helps to define image URI is loaded into View at
 	 * exact moment.
 	 */
@@ -155,12 +184,16 @@ class ImageLoaderEngine {
 	/**
 	 * Pauses engine. All new "load&display" tasks won't be executed until ImageLoader is {@link #resume() resumed}.<br
 	 * /> Already running tasks are not paused.
+	 * 暂停任务运行
 	 */
 	void pause() {
 		paused.set(true);
 	}
 
-	/** Resumes engine work. Paused "load&display" tasks will continue its work. */
+	/**
+	 * Resumes engine work. Paused "load&display" tasks will continue its work.
+	 * 任务恢复运行
+	 */
 	void resume() {
 		paused.set(false);
 		synchronized (pauseLock) {
@@ -169,6 +202,7 @@ class ImageLoaderEngine {
 	}
 
 	/**
+	 * 停止ImageLoader引擎,取消所有正在运行或者挂起的图片显示任务，并且清除内部的数据
 	 * Stops engine, cancels all running and scheduled display image tasks. Clears internal data.
 	 * <br />
 	 * <b>NOTE:</b> This method doesn't shutdown
@@ -182,7 +216,6 @@ class ImageLoaderEngine {
 		if (!configuration.customExecutorForCachedImages) {
 			((ExecutorService) taskExecutorForCachedImages).shutdownNow();
 		}
-
 		cacheKeysForImageAwares.clear();
 		uriLocks.clear();
 	}
