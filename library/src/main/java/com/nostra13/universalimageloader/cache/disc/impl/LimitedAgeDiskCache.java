43989d8e47b6bc98c7e1fbb7e45d5dceee9bbd92
jiangqqlmj <781931404@qq.com>
2015-12-25 11:30:48 +0800

    +):1.完成LimitedAgeDiskCache  缓存时间限制，如果文件超过最大缓存事件，会被删除。  缓存容量无限制类和方法的注释

--- library/src/main/java/com/nostra13/universalimageloader/cache/disc/impl/LimitedAgeDiskCache.java
+++ library/src/main/java/com/nostra13/universalimageloader/cache/disc/impl/LimitedAgeDiskCache.java
@@ -28,6 +28,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 缓存时间限制，如果文件超过最大缓存事件，会被删除。  缓存容量无限制。
+ * 当前类实现BaseDiskCache抽象类，进行功能扩展
  * Cache which deletes files which were loaded more than defined time. Cache size is unlimited.
  *
  * @author Sergey Tarasevich (nostra13[at]gmail[dot]com)
@@ -36,12 +38,14 @@ import java.util.Map;
 public class LimitedAgeDiskCache extends BaseDiskCache {
 
 	private final long maxFileAge;
-
+    //维护文件和文件修改的时间
 	private final Map<File, Long> loadingDates = Collections.synchronizedMap(new HashMap<File, Long>());
 
 	/**
+	 * 构造器  需要传入文件的最大缓存时间
 	 * @param cacheDir Directory for file caching
-	 * @param maxAge   Max file age (in seconds). If file age will exceed this value then it'll be removed on next
+	 * @param maxAge   时间的单位为秒级别.
+	 *                 Max file age (in seconds). If file age will exceed this value then it'll be removed on next
 	 *                 treatment (and therefore be reloaded).
 	 */
 	public LimitedAgeDiskCache(File cacheDir, long maxAge) {
@@ -49,6 +53,7 @@ public class LimitedAgeDiskCache extends BaseDiskCache {
 	}
 
 	/**
+	 * 构造器   需要传入缓存文件夹，备用缓存文件夹，以及最大的缓存时间
 	 * @param cacheDir Directory for file caching
 	 * @param maxAge   Max file age (in seconds). If file age will exceed this value then it'll be removed on next
 	 *                 treatment (and therefore be reloaded).
@@ -58,11 +63,19 @@ public class LimitedAgeDiskCache extends BaseDiskCache {
 	}
 
 	/**
+	 *
 	 * @param cacheDir          Directory for file caching
 	 * @param reserveCacheDir   null-ok; Reserve directory for file caching. It's used when the primary directory isn't available.
 	 * @param fileNameGenerator Name generator for cached files
 	 * @param maxAge            Max file age (in seconds). If file age will exceed this value then it'll be removed on next
 	 *                          treatment (and therefore be reloaded).
+	 */
+	/**
+	 * 构造器
+	 * @param cacheDir             缓存文件夹
+	 * @param reserveCacheDir      备用缓存文件夹
+	 * @param fileNameGenerator    缓存文件名 命名贵方
+	 * @param maxAge               文件最大缓存事件
 	 */
 	public LimitedAgeDiskCache(File cacheDir, File reserveCacheDir, FileNameGenerator fileNameGenerator, long maxAge) {
 		super(cacheDir, reserveCacheDir, fileNameGenerator);
@@ -86,7 +99,7 @@ public class LimitedAgeDiskCache extends BaseDiskCache {
 			} else {
 				cached = true;
 			}
-
+            //根据时间间隔进行判断 是否需要删除文件
 			if (System.currentTimeMillis() - loadingDate > maxFileAge) {
 				file.delete();
 				loadingDates.remove(file);
@@ -108,12 +121,13 @@ public class LimitedAgeDiskCache extends BaseDiskCache {
 	@Override
 	public boolean save(String imageUri, InputStream imageStream, IoUtils.CopyListener listener) throws IOException {
 		boolean saved = super.save(imageUri, imageStream, listener);
+		//时间保存
 		rememberUsage(imageUri);
 		return saved;
 	}
 
 	/**
-	 *
+	 * 进行文件保存缓存  同时文件修改时间维护
 	 * @param imageUri
 	 * @param bitmap
 	 * @return
@@ -122,26 +136,41 @@ public class LimitedAgeDiskCache extends BaseDiskCache {
 	@Override
 	public boolean save(String imageUri, Bitmap bitmap) throws IOException {
 		boolean saved = super.save(imageUri, bitmap);
+		//时间维护
 		rememberUsage(imageUri);
 		return saved;
 	}
 
+	/**
+	 * 根据图片URL地址进行删除缓存中的文件，同时删除维护的map对象
+	 * @param imageUri Image URI
+	 * @return
+	 */
 	@Override
 	public boolean remove(String imageUri) {
 		loadingDates.remove(getFile(imageUri));
 		return super.remove(imageUri);
 	}
 
+	/**
+	 * 清除缓存文件 同时清除维护的map对象
+	 */
 	@Override
 	public void clear() {
 		super.clear();
 		loadingDates.clear();
 	}
 
+	/**
+	 *
+	 * @param imageUri
+	 */
 	private void rememberUsage(String imageUri) {
 		File file = getFile(imageUri);
 		long currentTime = System.currentTimeMillis();
+		//修改文件最后修改事件
 		file.setLastModified(currentTime);
+		//保存文件修改事件
 		loadingDates.put(file, currentTime);
 	}
 }
