d0c924d5e8941676f134245610365e294c695bea
jiangqqlmj <781931404@qq.com>
2015-12-25 16:07:25 +0800

    +):1.完成MemoryCache的抽象实现类BaseMemoryCache类和相关方法的注释

--- library/src/main/java/com/nostra13/universalimageloader/cache/memory/BaseMemoryCache.java
+++ library/src/main/java/com/nostra13/universalimageloader/cache/memory/BaseMemoryCache.java
@@ -21,6 +21,7 @@ import java.lang.ref.Reference;
 import java.util.*;
 
 /**
+ * 基于MemoryCache实现抽象类，实现了内存缓存器中的相关公共方法
  * Base memory cache. Implements common functionality for memory cache. Provides object references (
  * {@linkplain Reference not strong}) storing.
  *
@@ -29,9 +30,17 @@ import java.util.*;
  */
 public abstract class BaseMemoryCache implements MemoryCache {
 
-	/** Stores not strong references to objects */
+	/**
+	 * Stores not strong references to objects
+	 * 进行维护图片信息的软引用map对象
+	 */
 	private final Map<String, Reference<Bitmap>> softMap = Collections.synchronizedMap(new HashMap<String, Reference<Bitmap>>());
 
+	/**
+	 * get(key)的具体方法实现，从软引用中根据key来获取图片
+	 * @param key
+	 * @return
+	 */
 	@Override
 	public Bitmap get(String key) {
 		Bitmap result = null;
@@ -42,12 +51,23 @@ public abstract class BaseMemoryCache implements MemoryCache {
 		return result;
 	}
 
+	/**
+	 * 根据key 和图片  把数据加入到软引用中
+	 * @param key
+	 * @param value
+	 * @return
+	 */
 	@Override
 	public boolean put(String key, Bitmap value) {
 		softMap.put(key, createReference(value));
 		return true;
 	}
 
+	/**
+	 * 根据key，从map对象中删除该对应的图片对象，并且返回该删除的图片
+	 * @param key
+	 * @return
+	 */
 	@Override
 	public Bitmap remove(String key) {
 		Reference<Bitmap> bmpRef = softMap.remove(key);
@@ -61,11 +81,17 @@ public abstract class BaseMemoryCache implements MemoryCache {
 		}
 	}
 
+	/**
+	 * 清除对象软引用map中的数据
+	 */
 	@Override
 	public void clear() {
 		softMap.clear();
 	}
 
-	/** Creates {@linkplain Reference not strong} reference of value */
+	/**
+	 * Creates {@linkplain Reference not strong} reference of value
+	 * 图片对象包装的引用对象  这边采用抽象方法，具体进行扩展，让具体的内存缓存器分别实现
+	 */
 	protected abstract Reference<Bitmap> createReference(Bitmap value);
 }
